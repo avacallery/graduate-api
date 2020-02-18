c43d4dac08bfef0c466657a90a43e37a630d24f1
avacallery <avacallery@gmail.com>
2020-02-18 12:10:10 -0500

    submit new student

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ mongoose.connect(
     .catch(err => console.error('Cannot connect to MongoDB.'));
 
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
 app.use('/api/graduates', graduates);
 app.use('/', frontend); 
 app.use(express.static('public')); 
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({extended: false})); 
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
