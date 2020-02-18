d3cd69a30825564da204e240f1834f1878a4c0ac
avacallery <avacallery@gmail.com>
2020-02-18 11:35:43 -0500

    fetch POST on index.html

--- public/app.js
+++ public/app.js
@@ -4,32 +4,21 @@ const bodyParser = require('body-parser')
 const app = express();
 const router = express.Router();
 
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html')
+// fetch('http://localhost:5000/api/graduates', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//         name: 'Ody the New Student',
+//         email: 'odywhycallery@gmail.com',
+//         dateOfGraduation: "04/20/2020",
+//         bio: "Cat school was not for me."
+//     })
 // })
 
-fetch('http://localhost:5000/api/graduates', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        name: 'NEW STUDENT',
-        email: 'AVANEWNEWNEW@gmail.com',
-        dateOfGraduation: "04/20/2020",
-        bio: "Blah blah blah."
-    })
-})
-    // })  .then(res => {
-    //         return res.json();
-    // })
-
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(error => console.error('ERROR!', error));
-
-
-// .then(data => console.log(data))
-//     .catch(error => console.error('ERROR!', error));
+    // .then(res => res.json())
+    // .then(json => console.log(json))
+    // .catch(error => console.error('ERROR!', error));
 
 module.exports = router;
